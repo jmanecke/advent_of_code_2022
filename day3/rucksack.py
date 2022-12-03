3db0099c1136783e059c6965f76b6871b9c1432e
John Manecke <john@manecke.com>
2022-12-03 16:10:40 -0500

    finished to get part 1 priority

--- day3/rucksack.py
+++ day3/rucksack.py
@@ -1,16 +1,62 @@
 ''' rucksack rearrange day 3 '''
 
-# pull in the list and strip off /n's, the break in half, each row become a list of three strings
+test_list = [
+	"vJrwpWtwJgWrhcsFMMfFFhFp",
+	"jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+	"PmmdzqPrVvPwwTWBwg",
+	"wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+	"ttgJtRGJQctTZtZT",
+	"CrZsJsPPZsGzwwsLwLmpwMDw"
+	]
+
+# pull in the list and strip off /n's, then break in half, each row become a list of three strings
+# also figure out what is in common
 def file_input(filename = 'input_day3.data'):
 	''' Reads in files and appends rows to list of rows '''
 	with open(filename, 'r') as infile:
-		filerawrows = infile.readlines()
-	filerows = []
-	for row in filerawrows:
-		wholestring = row.strip()
+		filerows = infile.read().splitlines()
+	return filerows
+
+
+def split_compartments(filerows):
+	''' for each row break it in half to left and right compartment contents'''
+	compartments = []
+	for wholestring in filerows:
 		left_compartment = wholestring[:int(len(wholestring)/2)]
 		right_comprtment = wholestring[int(len(wholestring)/2):]
-		filerows.append([wholestring, left_compartment, right_comprtment])
+		compartments.append([left_compartment, right_comprtment])
+	return compartments
 
-	return filerows
+
+def common_items(comp1, comp2):
+	''' provide two string (each compartments inventory) get back the item common '''
+	commonset = set(comp1)&set(comp2)
+	if len(commonset) != 1:
+		print("error, expecting only one commmon character")
+		return
+	# convert srings to sets then & together and get the single string in common
+	common_char = list(commonset)[0]
+	return common_char
+
+
+def get_priority(character):
+	''' given a character a-z or A-Z return integer priority based on rules '''
+	# validate 
+	if not character.isalpha():
+		print("invalid character input")
+		return 99999
+	if character.isupper():
+		priority = ord(character) - 64 + 26
+	else:
+		priority = ord(character) - 96
+	return priority
+
+def calc_p1_priority(filerows):
+	''' go through each row and sum priority '''
+	compartments = split_compartments(filerows)
+	priority_sum = 0
+	for sack in compartments:
+		common_char = common_items(sack[0], sack[1])
+		priority_sum += get_priority(common_char)
+	return priority_sum
 
