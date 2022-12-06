fbc598478e9a79381219b6f16f076e9d80987c8a
John Manecke <john@manecke.com>
2022-12-06 10:39:23 -0500

    final updates for part 1 solution

--- day6/signal.py
+++ day6/signal.py
@@ -2,24 +2,28 @@
 
 # writing the parameters here, could also just call from command line
 filename = 'input_day6.data'
-testfile = 'test_day6.data'
 
+# test data from the instructions
 teststream1 = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
 teststream2 = 'bvwbjplbgvbhsrlpgdmjqwftvncz'
 teststream3 = 'nppdvjthqldpwncqszvftbrmjlhg'
 teststream4 = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'
 teststream5 = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'
 
-def file_input(filename = 'input_day65ata'):
+def file_input(filename = 'input_day6.data'):
     ''' Reads in files and strips any /n '''
     with open(filename, 'r') as infile:
         filestream = infile.read().splitlines()
-        filestream.strip()
-    return filestream
+    return filestream[0]
 
 def find_unique_marker(inputstream, marker_length):
     ''' take input stream and look for a marker length characters wihtout repeat '''
     for i in range(0,len(inputstream)):
         if len(set(inputstream[i:i+marker_length])) == marker_length:
-            print("Marker at postition {}".format(i+marker_length))
             return i + marker_length
+
+def give_answer():
+    ''' just run this to spit out an answer '''
+    filestream = file_input()
+    marker_answer = find_unique_marker(filestream, 4)
+    print("The marker for part one is at: {}".format(marker_answer))
