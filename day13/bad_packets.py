560bc5e2107d536a2f5a90c92d57a09ee1edbfa4
John Manecke <jmanecke@topotag.com>
2022-12-18 11:12:26 -0500

    fixed bad handling of end of list

--- day13/bad_packets.py
+++ day13/bad_packets.py
@@ -26,25 +26,25 @@ def eval_packet_pairs(packet_dict):
 		# print()
 		# print("starting on index {}".format(i))
 		# print(packet_dict[i])
-		left_packet = packet_dict[i]['left'][:]
-		right_packet = packet_dict[i]['right'][:]
+		left_packet = packet_dict[i]['left']
+		right_packet = packet_dict[i]['right']
 		# print("    left: {}".format(left_packet))
 		# print("    right: {}".format(right_packet))
 		correct_order = list_check(left_packet, right_packet)
 		if correct_order == 1:
 			indices_sum += i
-			print("in correct order, sum is: {}".format(indices_sum))
+			#print("in correct order, sum is: {}".format(indices_sum))
 		elif correct_order == 2:
 			print("there is a problem")
-		else:
-			print("not in correct order")
+		#else:
+			#print("not in correct order")
 	return indices_sum
 
 
 def eval_single_packet(packet_dict, packet_index):
 	'''given a single packet index return the result'''
-	left_packet = packet_dict[packet_index]['left'][:]
-	right_packet = packet_dict[packet_index]['right'][:]	
+	left_packet = packet_dict[packet_index]['left']
+	right_packet = packet_dict[packet_index]['right']
 	correct_order = list_check(left_packet, right_packet)
 	if correct_order == 1:
 		print("in correct order")
@@ -58,30 +58,34 @@ def eval_single_packet(packet_dict, packet_index):
 def list_check(left, right):
 	''' takes two lists and checks for a next then pops and compares '''
 	# order - 0 - out of order, 1 - in order, 2 - indeterminate
-	print("starting list check - left: {}  right:{}".format(left, right))
+	#print("starting list check - left: {}  right:{}".format(left, right))
 	if type(left) != type(right) or type(left) != type(list()):
 		print("Something went wrong")
 	order = 2
 	while order == 2:
+		if len(left) == 0 and len(right) == 0:
+			# both ran out at same time so indeterminate
+			order = 2
+			return order
 		# rule 1 - if left runs out first then in right order
-		if len(left) != 0:
+		elif len(left) != 0:
 			if len(right) == 0:
 				#right ran out first
-				print("right ran out first")
+				#print("right ran out first")
 				order = 0
 				return order
 		else:
 			# left ran out first
-			print("left ran out first")
+			#print("left ran out first")
 			order = 1
 			return order
 		# two not empty lists of so take first value
 		# rule 3 if list take first value
 		subleft = left.pop(0)
 		subright = right.pop(0)
-		print("start list check left: {} right: {}".format(subleft, subright))
+		#print("start list check left: {} right: {}".format(subleft, subright))
 		order = check_popped_result(subleft, subright)
-		print("ended list check left: {} right: {} and order {}".format(subleft, subright, order))
+		#print("ended list check left: {} right: {} and order {}".format(subleft, subright, order))
 
 	return order
 
@@ -89,7 +93,7 @@ def list_check(left, right):
 def check_popped_result(left, right):
 	''' recursive function to check result or go deeper - returns order '''
 	# rule 2 - if both are integers left less than right is proper order
-	print("... checking popped - left: {}  right:{}".format(left, right))
+	#print("... checking popped - left: {}  right:{}".format(left, right))
 	if type(left) == type(right):
 		# same types
 		if type(left) == type(int()):
@@ -125,6 +129,11 @@ def check_popped_result(left, right):
 	return order
 
 
+def give_answer():
+	''' just run and print the answers '''
+	thepackets = file_input()
+	answer_p1 = eval_packet_pairs(thepackets)
+	print("the part 1 answer is: {}".format(answer_p1))
 
 
 
