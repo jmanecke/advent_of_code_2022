a73a58e92d40b035fb15ad9f7e6e7b3eb9993300
John Manecke <john@manecke.com>
2022-12-18 09:58:57 -0500

    adding new days

--- day13/bad_packets.py
+++ day13/bad_packets.py
@@ -1,5 +1,7 @@
 '''packets are out of order so need to parse the input and figure out which ones are no good'''
 
+import json
+
 # writing the parameters here, could also just call from command line
 filename = 'input_day13.data'
 testfile = 'test_day13.data'
@@ -11,35 +13,55 @@ def file_input(filename = 'input_day13.data'):
 	with open(filename, 'r') as infile:
 		packetlines = infile.read().splitlines()
 	thepackets = {}
-	for i in range (2,len(packetlines),3):
-		if len(packetlines[i]) != 0:
-			print("bad data")
-		else:
-			packetcontents = {"left": eval(packetlines[i-2]), "right": eval(packetlines[i-1])}
-			thepackets[int((i+1)/3)]=packetcontents
+	for i in range (2,len(packetlines)+3,3):
+		packetcontents = {"left": json.loads(packetlines[i-2]), "right": json.loads(packetlines[i-1])}
+		thepackets[int((i+1)/3)]=packetcontents
 	return thepackets
 
 
-def eval_packet_pairs(thepackets):
+def eval_packet_pairs(packet_dict):
 	''' go through the pairs of packet and check if they are in the right order '''
 	indices_sum = 0
-	for i in range(1,len(thepackets) + 1):
-		left_packet = thepackets[i]['left'][:]
-		right_packet = thepackets[i]['right'][:]
-		correct_order = list_check(left_packet, right_packet, 2)
+	for i in range(1,len(packet_dict) + 1):
+		# print()
+		# print("starting on index {}".format(i))
+		# print(packet_dict[i])
+		left_packet = packet_dict[i]['left'][:]
+		right_packet = packet_dict[i]['right'][:]
+		# print("    left: {}".format(left_packet))
+		# print("    right: {}".format(right_packet))
+		correct_order = list_check(left_packet, right_packet)
 		if correct_order == 1:
 			indices_sum += i
+			print("in correct order, sum is: {}".format(indices_sum))
 		elif correct_order == 2:
 			print("there is a problem")
+		else:
+			print("not in correct order")
 	return indices_sum
 
 
+def eval_single_packet(packet_dict, packet_index):
+	'''given a single packet index return the result'''
+	left_packet = packet_dict[packet_index]['left'][:]
+	right_packet = packet_dict[packet_index]['right'][:]	
+	correct_order = list_check(left_packet, right_packet)
+	if correct_order == 1:
+		print("in correct order")
+	elif correct_order == 2:
+		print("there is a problem")
+	else:
+		print("not in crrect order")
+	return correct_order
 
-def list_check(left, right, order):
+
+def list_check(left, right):
 	''' takes two lists and checks for a next then pops and compares '''
 	# order - 0 - out of order, 1 - in order, 2 - indeterminate
 	print("starting list check - left: {}  right:{}".format(left, right))
-
+	if type(left) != type(right) or type(left) != type(list()):
+		print("Something went wrong")
+	order = 2
 	while order == 2:
 		# rule 1 - if left runs out first then in right order
 		if len(left) != 0:
@@ -57,12 +79,14 @@ def list_check(left, right, order):
 		# rule 3 if list take first value
 		subleft = left.pop(0)
 		subright = right.pop(0)
-		order = check_popped_result(subleft, subright, 2)
+		print("start list check left: {} right: {}".format(subleft, subright))
+		order = check_popped_result(subleft, subright)
+		print("ended list check left: {} right: {} and order {}".format(subleft, subright, order))
 
 	return order
 
 
-def check_popped_result(left, right, order):
+def check_popped_result(left, right):
 	''' recursive function to check result or go deeper - returns order '''
 	# rule 2 - if both are integers left less than right is proper order
 	print("... checking popped - left: {}  right:{}".format(left, right))
@@ -80,21 +104,23 @@ def check_popped_result(left, right, order):
 
 		elif type(left) == type(list()):
 			# have two lists
-			order = list_check(left, right, 2)
+			order = list_check(left, right)
 
 	else:
 		# different types
 		# rule 4 - if different types - find integer and convert it to list then retry
 		if type(left) == type(int()):
-			newleft = [left]
-			newright = right
-			print("different type, making list of left: {}".format(newleft))
+			newleft = [left][:]
+			newright = right[:]
+			#print("different type, making list of left: {}".format(newleft))
+		elif type(right) == type(int()):
+			newright = [right][:]
+			newleft = left[:]
+			#print("different type, making list of right: {}".format(newright))
 		else:
-			newright = [right]
-			newleft = left
-			print("different type, making list of right: {}".format(newright))
+			print("something went wrong")
 		# retry with new lists
-		order = list_check(newleft, newright, 2)
+		order = list_check(newleft, newright)
 
 	return order
 
